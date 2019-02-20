f3284bbf240c6879821f724316e641ba732363ee
Doni Tampubolon <dtampubolon31@gmail.com>
2019-02-20 03:08:05 -0500

    Lab 6 commit 2:
    PYTHON: MqttClientConnector-> finished writing code. testing is next
    MqttPubClientTestApp in progress

--- apps/labs/module06/MqttClientConnector.py
+++ apps/labs/module06/MqttClientConnector.py
@@ -30,12 +30,31 @@ class MqttClientConnector():
         self.client.on_message = on_message
         self.client.on_publish = on_publish
         self.client.on_subscribe = on_subscribe
-        self.client.on_log = on_log
+        #self.client.on_log = on_log
             
+    def start(self):
+        print("Starting loop..")
+        self.client.loop_start()
             
     def connect(self):
-        print("Connecting to broker: " +str(self.hostname))
+        print("Connecting to broker: " + str(self.hostname))
+        self.client.connect(self.hostname,self.port)
+    
+    def disconnect(self):
+        print("Disconnecting from broker: " + str(self.hostname))
+        self.client.disconnect()
         
+    def publish(self, topic, message):
+        print("Publishing message..")
+        self.client.publish(topic, message)
+        
+    def subscribe(self, topic, qos):   
+        print("Subscribing to topic: " + topic)
+        self.client.subscribe(topic, qos)
+        
+    def unsubscribe(self, topic):
+        print("Unsubscribing from topic: " + topic)
+        self.client.unsubscribe(topic)
         
 #Callback functions
 
@@ -58,25 +77,28 @@ This function is called when the client disconnects form the server
 def on_disconnect(client, userdata, rc):
     if rc == 0:
         print("Succesfully disconnected from broker")
-        
     else:
         print("Unexpected disconnection \tReturned code: " + rc)
-'''
-This function is called when a subscriber client receives a publishes msg 
-'''        
+    client.loop_stop()
+
+#This function is called when a subscriber client receives a publishes msg 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    
+
+#This function is called when a message is published    
 def on_publish(client, userdata, mid):
     print("A message has been successfully published")
     print("mid " + str(mid))
-    
+
+#This function is called when a new subscription is made    
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscription successful..")
     print("Message id: " + str(mid) + "\tGranted QoS: " + str(granted_qos))
 
+#This function is called when unsubscription is done
 def on_unsubscribe(client, userdata, mid):
     print("Unsubscribed successfully")
 
+#This function is called when the client has log information for debugging
 def on_log(client, userdata, level, buf):
     print(buf)
--- apps/labs/module06/MqttPubClientTestApp.py
+++ apps/labs/module06/MqttPubClientTestApp.py
@@ -4,14 +4,28 @@ Created on Feb 19, 2019
 @author: Doni Tampubolon
 '''
 
+from labs.module06 import MqttClientConnector
+from labs.common import SensorData
+
 class MqttPubClientTestApp(object):
     '''
-    classdocs
+    Instances of this class publish messages to a connected MQTT broker
+    MQTT broker will then publish them to subscribed clients
     '''
-
 
     def __init__(self, params):
         '''
         Constructor
         '''
+        self.topic = "Test-CSYE6530"
+        self.payload = "This is a test"
+        self.name = "Client1"
+        self.connector = MqttClientConnector.MqttClientConnector(self.name)
+        self.sensorData = SensorData.SensorData()
+    
+    def main(self):
+        self.connector.connect()
+        self.connector.start()
         
+    if __name__ == '__main__':
+        main()
