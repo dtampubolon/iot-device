9ade2960ddc1406abe9269a7b2f79fe34c4428f4
Doni Tampubolon <dtampubolon31@gmail.com>
2019-04-17 18:09:25 -0400

    Python: Final Project
    Added new features to ConstrainedDeviceApp
    Created a new temp sensor adaptor

--- apps/project/ConstrainedDeviceApp.py
+++ apps/project/ConstrainedDeviceApp.py
@@ -8,20 +8,14 @@ sys.path.append('/home/pi/workspace/iot-device/apps')
 
 from time import sleep
 from project import PitchAdaptor
+from project import TempSensAdapt
 from labs.module06 import MqttClientConnector
 from labs.common import DataUtil
 from labs.common.PitchData import PitchData
 
-#pitch adaptor configuration
-pitchAdaptor = PitchAdaptor.PitchAdaptor()
-pitchAdaptor.daemon = True
-pitchAdaptor.setEnable(True)
-pitchAdaptor.start()
-period = 30 #time interval in seconds to send reading to gateway
-threshold = 310 #Minimum pitch in degrees that triggers app to send current reading to gateway
-
 #MQTT connection configuration:
-topic = "PitchData-CSYE6530"
+pubTopic = "PitchData-CSYE6530"
+pubTopic2 = "Temperature-CSYE6530"
 payload = ""
 name = "PitchSensor-CSYE6530"
 connector = MqttClientConnector.MqttClientConnector(name)
@@ -29,17 +23,56 @@ dataUtil = DataUtil.DataUtil()
 connector.connect("iot.eclipse.org", 1883)
 connector.loop_start()
 
+#pitch adaptor configuration
+pitchAdaptor = PitchAdaptor.PitchAdaptor()
+pitchAdaptor.daemon = True
+pitchAdaptor.setEnable(True)
+pitchAdaptor.start()
+period = 10 #time interval in seconds to send reading to gateway
+threshold = 310 #Minimum pitch in degrees that triggers app to send current reading to gateway
+
+
+#temperature sensor adaptor configuration
+tempSensorAdaptor = TempSensAdapt.TempSensorAdapt(connector)
+tempSensorAdaptor.daemon = True
+tempSensorAdaptor.setEnable(True)
+tempSensorAdaptor.start()
+maxTemp = 25
+
+count=0
+
 while(True):
     pitch = pitchAdaptor.getPitchData()
     curVal = pitch.getValue()
-    print(curVal)
-    if(curVal<=threshold):
+    print("Pitch: " + str(curVal))
+    
+    sensorData = tempSensorAdaptor.getSensorData()
+    sensorVal = sensorData.getValue()
+    
+    if(curVal<=threshold and count%period!=0):
         #Sends MQTT message to alert the gateway device which is subscribed to the topic if the pitch is below the threshold
-        print("WARNING: current pitch is below threshold, sending alert to gateway\n")
+        print("ALERT: Pitch angle is below threshold")
         pd = PitchData()
         pd.setValue(curVal)
         payload = dataUtil.pitchDataToJson(pd)
-        connector.publish(topic, payload)
-    else:
-        sleep(1)
+        connector.publish(pubTopic, payload)
+
+    elif count%10==0:
+        #Sending new MQTT message every period of seconds if no threshold is exceeded 
+        print("Constrained Device: Sending new pitch reading to gateway..")
+        pd = PitchData()
+        pd.setValue(curVal)
+        payload = dataUtil.pitchDataToJson(pd)
+        connector.publish(pubTopic, payload)
+        
+    if(sensorVal<=maxTemp and count%period!=0): 
+        #Sending new MQTT message every period of seconds if no threshold is exceeded 
+        print("Constrained Device: Sending new temperature reading to gateway..")
+        payload2 = dataUtil.sensorDataToJson(sensorData)
+        connector.publish(pubTopic2, payload2)
+        
+    count+=1
+    sleep(1)
     
+def updateTemperature(newVal):
+    return newVal
---
+++ apps/project/TempSensAdapt.py
@@ -0,0 +1,72 @@
+'''
+Created on Jan 19, 2019
+
+@author: Doni Tampubolon
+NUID: 001400708
+CSYE 6530 - Connected Devices
+Final Project
+'''
+import sys
+sys.path.append('/home/pi/workspace/iot-device/apps')
+
+import threading
+import time
+
+from labs.common import SensorData
+from labs.common import ConfigConst
+from labs.common import ConfigUtil
+from labs.common import DataUtil
+from labs.module06 import MqttClientConnector
+from sense_hat import SenseHat
+
+class TempSensorAdapt(threading.Thread):
+    '''
+    This class reads temperature data from sensehat
+    and sends email notifications if changes beyond the threshold occur. 
+    '''
+    def __init__(self, connector, period=1, threshold=25):
+        '''
+        Constructor
+        '''
+        threading.Thread.__init__(self)
+        self.period = period #time interval in seconds
+        self.threshold = threshold #Threshold that triggers app to send an alert to user
+        self.enable = False
+        self.sense = SenseHat()
+        self.pubTopic = "Temperature-CSYE6530"
+        self.sensorData = SensorData.SensorData()
+        self.connector = connector #MQTT connector
+        
+        self.isPrevTempSet = False #Before the sensor adaptor is started, no previous temperature readings i.e. no average temp
+        
+        self.config = ConfigUtil.ConfigUtil(r'C:\Users\Doni Tampubolon\Documents\Grad School\CSYE6530\gitrepo\iot-device\apps\labs\data\ConnectedDevicesConfig.props')
+        self.config.loadConfig()
+        print('Configuration data...\n' + str(self.config))
+        
+        self.nominalTemp = float(self.config.getProperty(ConfigConst.CONSTRAINED_DEVICE, ConfigConst.NOMINAL_TEMP_KEY))
+        self.dataUtil = DataUtil.DataUtil()
+        
+    def run(self):
+        while True:
+            if self.enable:
+                self.curTemp = self.sense.get_temperature()
+                self.sensorData.addValue(self.curTemp)
+                
+                print("\n----------")
+                print("New Sensor Readings")
+                print(" " + str(self.sensorData))
+                
+                if self.isPrevTempSet == False:
+                    self.prevTemp = self.curTemp #Initial temp is set to the first current temp
+                    self.isPrevTempSet = True
+                else:
+                    if(self.curTemp>=self.threshold):
+                        print("ALERT: Temperature exceeds the maximum!")
+                        self.connector.publish(self.pubTopic)
+                time.sleep(self.period)
+                    
+    def setEnable(self, enable):
+        self.enable = enable
+        
+    def getSensorData(self):
+        return self.sensorData
